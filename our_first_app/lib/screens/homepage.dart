3388d5dcb3d700e4c199bae47146f0e5e24d060a
lorenzocester <lorenzo.cester@studenti.unipd.it>
2022-04-29 19:32:12 +0200

    [main]

--- our_first_app/lib/screens/homepage.dart
+++ our_first_app/lib/screens/homepage.dart
@@ -34,100 +34,149 @@ final Color col = const Color.fromARGB(248, 159, 224, 159);
         ],
       ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.all(15),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            FittedBox(
-              child: Row(
-                children: [
-                  GestureDetector(
-                        child: Container(
-                          child: const Icon(MdiIcons.heartPulse, size: 50, color: Colors.white),
-                          decoration: BoxDecoration(
-                            color: col,
-                            borderRadius: BorderRadius.circular(40)
+          children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+                      child: GestureDetector(
+                            child: Container(
+                              width: 140,
+                              height: 140,
+                              child: const Icon(MdiIcons.heartPulse, size: 50, color: Colors.white),
+                              decoration: BoxDecoration(
+                                color: col,
+                                borderRadius: BorderRadius.circular(40)
+                              ),
+                            ),
+                            onTap: (){}
                           ),
-                        ),
-                        onTap: (){}
-                      ),
-                      GestureDetector(
-                        child: Container(
-                          child: const Icon(MdiIcons.run, size: 50, color: Colors.white),
-                          decoration: BoxDecoration(
-                            color: col,
-                            borderRadius: BorderRadius.circular(40)
-                          ),
-                        ),
-                        onTap: (){}
-                      )
-                ]
-              ),
-            ),
-            FittedBox(
-              child: Row(
-                children: [
-                  GestureDetector(
-                        child: Container(
-                          child: const Icon(MdiIcons.bed, size: 50, color: Colors.white),
-                          decoration: BoxDecoration(
-                            color: col,
-                            borderRadius: BorderRadius.circular(40)
-                          ),
-                        ),
-                        onTap: (){}
-                      ),
-                      GestureDetector(
-                        child: Container(
-                          child: const Icon(MdiIcons.emoticon, size: 50, color: Colors.white),
-                          decoration: BoxDecoration(
-                            color: col,
-                            borderRadius: BorderRadius.circular(40)
-                          ),
-                        ),
-                        onTap: (){}
-                      ),
-                ]
-              ),
-            ),
-            FittedBox(
-              child: Row(
-                children: [
-                  GestureDetector(
-                        child: Container(
-                          child: const Icon(MdiIcons.silverwareForkKnife, size: 50, color: Colors.white),
-                          decoration: BoxDecoration(
-                            color: col,
-                            borderRadius: BorderRadius.circular(40)
-                          ),
-                        ),
-                        onTap: (){}
-                      ),
-                      GestureDetector(
-                        child: Container(
-                          child: const Icon(MdiIcons.lungs, size: 50, color: Colors.white),
-                          decoration: BoxDecoration(
-                            color: col,
-                            borderRadius: BorderRadius.circular(40)
-                          ),
-                        ),
-                        onTap: (){}
-                      )
-                ]
-              ),
-            ),
-            GestureDetector(
-                  child: Container(
-                    child: const Icon(MdiIcons.yoga, size: 50, color: Colors.white),
-                    decoration: BoxDecoration(
-                      color: col,
-                      borderRadius: BorderRadius.circular(40)
                     ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                          child: GestureDetector(
+                            child: Container(
+                              width: 140,
+                              height: 140,
+                              child: const Icon(MdiIcons.run, size: 50, color: Colors.white),
+                              decoration: BoxDecoration(
+                                color: col,
+                                borderRadius: BorderRadius.circular(40)
+                              ),
+                            ),
+                            onTap: (){}
+                          ),
+                        )
+                  ]
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+                      child: GestureDetector(
+                            child: Container(
+                              width: 140,
+                              height: 140,
+                              child: const Icon(MdiIcons.bed, size: 50, color: Colors.white),
+                              decoration: BoxDecoration(
+                                color: col,
+                                borderRadius: BorderRadius.circular(40)
+                              ),
+                            ),
+                            onTap: (){}
+                          ),
+                    ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                          child: GestureDetector(
+                            child: Container(
+                              width: 140,
+                              height: 140,
+                              child: const Icon(MdiIcons.batteryAlert, size: 50, color: Colors.white),
+                              decoration: BoxDecoration(
+                                color: col,
+                                borderRadius: BorderRadius.circular(40)
+                              ),
+                            ),
+                            onTap: (){}
+                          ),
+                        ),
+                  ]
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+                      child: GestureDetector(
+                            child: Container(
+                              width: 140,
+                              height: 140,
+                              child: const Icon(MdiIcons.silverwareForkKnife, size: 50, color: Colors.white),
+                              decoration: BoxDecoration(
+                                color: col,
+                                borderRadius: BorderRadius.circular(40)
+                              ),
+                            ),
+                            onTap: (){}
+                          ),
+                    ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                          child: GestureDetector(
+                            child: Container(
+                              width: 140,
+                              height: 140,
+                              child: const Icon(MdiIcons.lungs, size: 50, color: Colors.white),
+                              decoration: BoxDecoration(
+                                color: col,
+                                borderRadius: BorderRadius.circular(40)
+                              ),
+                            ),
+                            onTap: (){}
+                          ),
+                        )
+                  ]
+                ),
+              ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+              child: GestureDetector(
+                    child: Container(
+                      width: 320,
+                      height: 140,
+                      child: const Icon(MdiIcons.yoga, size: 50, color: Colors.white),
+                      decoration: BoxDecoration(
+                        color: col,
+                        borderRadius: BorderRadius.circular(40)
+                      ),
+                    ),
+                    onTap: (){}
                   ),
-                  onTap: (){}
-                )
+            )
           ]
         ),
-      )     
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: IconButton(icon: const Icon(MdiIcons.home), onPressed: (){},), label: 'Home'),
+          BottomNavigationBarItem(icon: IconButton(icon: const Icon(MdiIcons.account), onPressed: (){},), label: 'Profile')
+        ]
+      ),
     );
   }
 }
