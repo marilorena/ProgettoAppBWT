4ffcbb911c80eb67bd9a071ae9afa0a41b49d3b7
marilorena <marialorenamorelli@outlook.it>
2022-05-17 11:18:36 +0200

    Update yogapage.dart

--- our_first_app/lib/secondary_screens/yogapage.dart
+++ our_first_app/lib/secondary_screens/yogapage.dart
@@ -12,7 +12,7 @@ class YogaPage extends StatelessWidget{
 
  @override 
  Widget build(BuildContext context){
-   _fetchImage();
+   
    return Scaffold(
      body: Center(
        child: 
@@ -23,11 +23,13 @@ class YogaPage extends StatelessWidget{
                 final pose = snapshot.data as YogaPose;
                 return Card(
                       child: Column( 
+                        mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Text(pose.name),
                               Text(pose.sanskritname, style: TextStyle(fontStyle: FontStyle.italic, color: Colors.green),
                               ),
                               Image.network(pose.imageurl)
+                              
                             ],
                       ),
                     );
@@ -52,12 +54,13 @@ class YogaPage extends StatelessWidget{
   return response.statusCode == 200 ? YogaPose.fromJson(jsonDecode(response.body)): null;
   
  }
-  Future<void> _fetchImage() async {
-  
-  final url = 'https://www.dropbox.com/s/4m64ztxkj8a4dab/boatstraightlegs.svg?raw=1';
-  final response = await http.get(Uri.parse(url));
-  print(response.body);
 
-}
+  //Future<void> _fetchImage() async {
+  
+ //final url = 'https://uc549dab9f5a20837e1793111b7b.dl.dropboxusercontent.com/cd/0/inline2/BlbzVO9n-ugBaXcqjezjxw3IKcrB1Zm2rU-Bnx_82xSf7szRATEvCCGYEsIf8pQLgIN4rRb2WSvPfxHutblHjpWYau-m8dmHsNRaj0hJoufi9hzZHq7q5c6ZEgj5x39xekx9-P6-67aUBG4GSRh552v75sE91KghsGknR6buf6t3Rxz8Z9hPCfrYZAZ6htxyMSPP6yxOt3_P7jd-gOM4OfwxAT5TFcrf4BjSKSDJByKJarjFCg-8NZJGz5Q-kHUJI_e0k4wgF_w25BVWL6aiF1FlEiwde2fS2NNgbLVaqf6kXE1IziVllnGO7BQkUryp9A1IZ1mgz-DLdM8gGF4tfxE6yLe7EydvrbI2cFUtTwnkZVErT05FUg7TSiW68Q8ZHS2evQu0Z6Mrzaz2PrXiGkrkPBBew6k-f7_ORi_7ekK7pg/file';
+ // final response = await http.get(Uri.parse(url));
+ // print(response.body);
+
+//}
 
 }
